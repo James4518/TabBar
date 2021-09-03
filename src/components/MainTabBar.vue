e33cd8c54fc070b58c845c93a2a8627126284bc3
‘gcd’ <it_gcd@163.com>
2021-09-04 05:36:59 +0800

    初建雏形（排版+样式）

---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <tab-bar>
+    <tab-bar-item>
+      <img slot="item-icon" src="../assets/img/tabbar/home.svg">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="../assets/img/tabbar/category.svg">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="../assets/img/tabbar/shopcart.svg">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="../assets/img/tabbar/profile.svg">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from '../components/tabbar/TabBar.vue'
+  import TabBarItem from '../components/tabbar/TabBarI-item.vue'
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
